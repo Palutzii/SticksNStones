b770dae6488e336b0b15b008f336e74715886cb7
Pavel Sarmiento <pavelsarmiento@hotmail.com>
2022-07-08 17:27:02 +0200

    ADD:  MatchMaking Server following Marcs loom video
    This will be worked on more with my own code after learning more from marcs loom tutorial

--- SticksNStonesServer/Program.cs
+++ SticksNStonesServer/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using SticksNStonesServer;
 
-namespace SticksNStonesServer
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+
+var tcpListener = new TcpListener(IPAddress.Any, 12244);
+tcpListener.Start();
+
+SticksNStonesMatch match = null;
+
+while (true){
+    Console.WriteLine("Waiting for connections...");
+    // Wait for a client to establish a connection and return that connection
+    var tcpClient = tcpListener.AcceptTcpClient();
+    
+    Console.WriteLine($"Client {tcpClient.Client.RemoteEndPoint} connected!");
+
+    if (match == null){
+        Console.WriteLine("Starting new Match for Player. Waiting for Second Player");
+        match = new SticksNStonesMatch{
+            player1 = tcpClient
+        };
+    }
+    else{
+        Console.WriteLine("Assigning PLayer to existing Match. Starting Match");
+        match.player2 = tcpClient;
+        new Thread(match.Start).Start();
+        match = null;
     }
 }

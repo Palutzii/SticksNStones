cb918806da0145e8d5191e65540c8f48e7787b3b
Pavel Sarmiento <pavelsarmiento@hotmail.com>
2022-07-21 15:02:43 +0200

    UPDATE: Added comments on what the script does

--- SticksNStonesServer/SticksNStonesServer/Model/PlayerDataBase.cs
+++ SticksNStonesServer/SticksNStonesServer/Model/PlayerDataBase.cs
@@ -3,6 +3,16 @@ using StickNStonesShared.StickNStonesShared.Model;
 
 namespace SticksNStonesServer.Model;
 
+/// <summary>
+///  Simple DataBase
+///  It can store Entities (Data Objects that are Identifiable, that have an ID)
+///  They are compared by ID (e.g. Player("Pavel", 200) == Player("Pavel", 300) because the id (the name) are the same)
+///  Value Objects are compared by Value( e.g. Vector(2,2) == Vector(2,2), because all field are the same)
+///
+///  It can also retrieve Entities
+///  It-Memory Database (therefore stored in RAM)
+///  Therefore, this Database is non-persistent
+/// </summary>
 public class PlayerDataBase{
     readonly Dictionary<string, PlayerData> _playerDatas = new ();
 
